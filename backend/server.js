55f83b4d6e9a51b2a91b2fc0b293443bae1e0ad5
Swaye Chateau <swaye@kitechltd.com>
2020-05-06 18:03:14 +0100

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,69 @@
+// const path = require('path')
+const PORT = process.env.PORT || 3000
+const botName = 'ChatCord Bot'
+
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+const formatMessage = require('./utils/messages')
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
+} = require('./utils/users')
+
+app.get('/', (req, res) => {
+  // res.send({ message: 'hello world!' })
+  res.sendFile(__dirname + '/index.html')
+})
+
+// Set static folder
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when client connects
+io.on('connection', socket => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room)
+    socket.join(user.room)
+    // Welcome current user
+    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'))
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} has joined the chat`)
+      )
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room)
+    })
+  })
+
+  // Listen for chatMessage
+  socket.on('chatMessage', msg => {
+    const user = getCurrentUser(socket.id);
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  })
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id)
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(botName, `${user.username} has left the chat`)
+      )
+      // Send users and room info
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      })
+    }
+  })
+})
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
